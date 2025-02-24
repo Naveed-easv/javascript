c728ecd47bc9e3beebdf45dbc9162bddd90613df
Naveed-easv <navnou01@easv365.dk>
2025-02-24 11:14:46 +0100

    Calculate total price and add suffixes

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 let product1Name = "Bord"
-let product1Price = 5000
+let product1Price = 1000
 let product1Amount = 5
 console.log("Produkt 1:",product1Name)
-console.log("Pris:",product1Price)
-console.log("Antal:",product1Amount)
+
+let product1TotalPrice = product1Price * product1Amount
+console.log("Pris:",product1TotalPrice,"kr.")
+console.log("Antal:",product1Amount,"stk.")
 
 let product2Name = "Stol"
-let product2Price = 2500
+let product2Price = 250
 let product2Amount = 10
 console.log("Produkt 2:",product2Name)
-console.log("Pris:",product2Price)
-console.log("Antal:",product2Amount)
+
+let product2TotalPrice = product2Price * product2Amount
+console.log("Pris:",product2TotalPrice,"kr.")
+console.log("Antal:",product2Amount,"stk.")
 
 let totalProductAmount = product1Amount + product2Amount
-console.log("Samlet antal:",totalProductAmount)
+console.log("Samlet antal:",totalProductAmount,"stk.")
 
-let totalProductPrice = product1Price + product2Price
-console.log("Samlet pris:",totalProductPrice)
+let totalProductPrice = product1TotalPrice + product2TotalPrice
+console.log("Samlet pris:",totalProductPrice,"kr.")
