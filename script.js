d63f00b347cb01cb16c3f8d7d65b55190f470acb
Naveed-easv <navnou01@easv365.dk>
2025-02-24 10:51:59 +0100

    Create script

---
+++ script.js
@@ -0,0 +1,19 @@
+let product1Name = "Bord"
+let product1Price = 5000
+let product1Amount = 5
+console.log("Produkt 1:",product1Name)
+console.log("Pris:",product1Price)
+console.log("Antal:",product1Amount)
+
+let product2Name = "Stol"
+let product2Price = 2500
+let product2Amount = 10
+console.log("Produkt 2:",product2Name)
+console.log("Pris:",product2Price)
+console.log("Antal:",product2Amount)
+
+let totalProductAmount = product1Amount + product2Amount
+console.log("Samlet antal:",totalProductAmount)
+
+let totalProductPrice = product1Price + product2Price
+console.log("Samlet pris:",totalProductPrice)
