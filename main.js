6f323effb7f62f91374dc803f67479eefab6b67c
Naveed-easv <navnou01@easv365.dk>
2025-03-02 21:14:56 +0100

    Update image source to use an index and add semicolons

--- main.js
+++ main.js
@@ -6,19 +6,25 @@ let images = [
     "media/white-back.png",
     "media/white-side.png"
 ]
+
+// image
+let imageIndex = 0;
 let image = document.getElementById("productImage");
 let productSaleEl = document.getElementById("productSale")
 
+function updateImage() {
+    image.src = images[imageIndex];
+}
+
 // update price continously
 function updatePrice() {
-    if (color === "black") {
-        if (size === "medium" || size === "large") {
+    if (color === "black" && (size === "medium" || size === "large")) { // check parameters for when to change the price
             productPriceEl.innerText = 75 + ",-";
-        }
     } else {
         productPriceEl.innerText = 100 + ",-";
     }
-    if (productPriceEl.innerText === "75,-") {
+    // display "on sale" message
+    if (productPriceEl.innerText === "75,-") { 
         productSaleEl.style.display = "block";
     } else {
         productSaleEl.style.display = "none";
@@ -26,34 +32,36 @@ function updatePrice() {
 }
 
 // colors
-let color = "black"
-let productColorEl = document.getElementById("productColor")
-productColorEl.innerText = "Sort"
+let color = "black";
+let productColorEl = document.getElementById("productColor");
+productColorEl.innerText = "Sort";
 
 function black() {
-    color = "black"
-    productColorEl.innerText = "Sort"
-    image.src = images[0]
-    updatePrice()
+    color = "black";
+    productColorEl.innerText = "Sort";
+    imageIndex = 0;
+    updateImage();
+    updatePrice();
 }
 
 function white() {
-    color = "white"
-    image.src = images[3]
-    productColorEl.innerText = "Hvid"
+    color = "white";
+    imageIndex = 3;
+    productColorEl.innerText = "Hvid";
     productPriceEl.innerText = 100 + ",-";
-    updatePrice()
+    updateImage();
+    updatePrice();
 }
 
 // price
-let productPriceEl = document.getElementById("productPrice")
+let productPriceEl = document.getElementById("productPrice");
 let productPrice = 100 + ",-";
-productPriceEl.innerText = productPrice
+productPriceEl.innerText = productPrice;
 
 // sizes
-let size = "small"
-let productSizeEl = document.getElementById("productSize")
-productSizeEl.innerText = "Small"
+let size = "small";
+let productSizeEl = document.getElementById("productSize");
+productSizeEl.innerText = "Small";
 
 function small() {
     size = "small"
@@ -63,17 +71,17 @@ function small() {
     } else if (color === "black" && size === "small") {
         productPriceEl.innerText = 100 + ",-";
     }
-    updatePrice()
+    updatePrice();
 }
 
 function medium() {
-    size = "medium"
-    productSizeEl.innerText = "Medium"
-    updatePrice()
+    size = "medium";
+    productSizeEl.innerText = "Medium";
+    updatePrice();
 }
 
 function large() {
-    size = "large"
-    productSizeEl.innerText = "Large"
-    updatePrice()
+    size = "large";
+    productSizeEl.innerText = "Large";
+    updatePrice();
 }
