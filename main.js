7f0215b2af0aab2648cb52655da887a52d5a0978
Naveed-easv <navnou01@easv365.dk>
2025-03-03 13:28:50 +0100

    Begin work on category site

---
+++ main.js
@@ -0,0 +1,8 @@
+const product = [
+    { name: "Product 1", price: 10, image: "media\black-front.png", available: true },
+    { name: "Product 2", price: 15, image: "media\white-front.png", available: false },
+    { name: "Product 3", price: 20, image: "media\black-front.png", available: true },
+    { name: "Product 4", price: 5, image: "media\black-front.png", available: false },
+    { name: "Product 5", price: 25, image: "media\white-front.png", available: true },
+    { name: "Product 6", price: 30, image: "media\black-front.png", available: true },
+]
