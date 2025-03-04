58ad7042f38eb762080bc02cdfcaa2300fdf81aa
Naveed-easv <navnou01@easv365.dk>
2025-03-04 15:43:34 +0100

    Add more products to show dynamic content

--- main.js
+++ main.js
@@ -5,6 +5,16 @@ const products = [
     { name: "Product 4", price: 5, image: "media/black-front.png", available: false },
     { name: "Product 5", price: 25, image: "media/white-front.png", available: true },
     { name: "Product 6", price: 30, image: "media/black-front.png", available: true },
+    // { name: "Product 7", price: 10, image: "media/black-front.png", available: true },
+    // { name: "Product 8", price: 15, image: "media/white-front.png", available: false },
+    // { name: "Product 9", price: 20, image: "media/black-front.png", available: true },
+    // { name: "Product 10", price: 10, image: "media/black-front.png", available: true },
+    // { name: "Product 11", price: 15, image: "media/white-front.png", available: false },
+    // { name: "Product 12", price: 20, image: "media/black-front.png", available: true },
+    // { name: "Product 13", price: 10, image: "media/black-front.png", available: true },
+    // { name: "Product 14", price: 15, image: "media/white-front.png", available: false },
+    // { name: "Product 15", price: 20, image: "media/black-front.png", available: true },
+    // { name: "Product 16", price: 10, image: "media/black-front.png", available: true },
 ]
 
 const productsEl = document.getElementById("products")
@@ -20,7 +30,7 @@ for (const product of products) {
     <h2 class="product__title">
         <span id="productTitle">${product.name}</span></h2>
     <p class="product__price">
-        Pris: <span id="productPrice">$${product.price}</span>
+        Price: <span id="productPrice">$${product.price}</span>
     </p>`;
     productsEl.appendChild(productArticle);
 }
