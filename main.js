598e75f8df0f76deb99a75af24a4ba9591500454
Naveed-easv <navnou01@easv365.dk>
2025-03-10 13:28:10 +0100

    Continue work

--- main.js
+++ main.js
@@ -1,12 +1,23 @@
-let gums = [
-    { name: "Aftertaste", image: "media\aftertaste.png" },
-    { name: "Burned Out", image: "media\burned_out.png"},
-    { name: "Dead of Nuclear Winter", image: "media\dead_of_nuclear_winter.png"},
-    { name: "Ephemeral Enhancement", image: "media\ephemeral_enhancement.png"},
-    { name: "I'm Feeling Lucky", image: "media\im_feeling_lucky.png"},
-    { name: "Immolation Liquidation", image: "media\immolation_liquidation.png"},
-    { name: "Licensed Contractor", image: "media\licensed_contractor.png"},
-    { name: "Phoenix Up", image: "media\phoenix_up.png"},
-    { name: "Pop Shocks", image: "media\pop_shocks.png"},
-    { name: "Respin Cycle", image: "media\respin_cycle.png"}
-]
+const gums = {
+    gum1: { name: "Aftertaste", image: "media/aftertaste.png" },
+    gum2: { name: "Burned Out", image: "media/burned_out.png"},
+    gum3: { name: "Dead of Nuclear Winter", image: "media/dead_of_nuclear_winter.png"},
+    gum4: { name: "Ephemeral Enhancement", image: "media/ephemeral_enhancement.png"},
+    gum5: { name: "I'm Feeling Lucky", image: "media/im_feeling_lucky.png"},
+    gum6: { name: "Immolation Liquidation", image: "media/immolation_liquidation.png"},
+    gum7: { name: "Licensed Contractor", image: "media/licensed_contractor.png"},
+    gum8: { name: "Phoenix Up", image: "media/phoenix_up.png"},
+    gum9: { name: "Pop Shocks", image: "media/pop_shocks.png"},
+    gum10: { name: "Respin Cycle", image: "media/respin_cycle.png"}
+}
+
+
+for (let key in gums) {
+    let nameEl = document.getElementById(`${key}__name`);
+    let imageEl = document.getElementById(`${key}__image`);
+
+    if (nameEl) nameEl.innerText = gums[key].name;
+    if (imageEl) imageEl.src = gums[key].image;
+}
+
+// titles 
