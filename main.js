8a5cff2b82a1999d89e68e4b870e8229e144a727
Naveed-easv <navnou01@easv365.dk>
2025-03-10 11:32:12 +0100

    Begin work on gobblegum tracker

---
+++ main.js
@@ -0,0 +1,12 @@
+let gums = [
+    { name: "Aftertaste", image: "media\aftertaste.png" },
+    { name: "Burned Out", image: "media\burned_out.png"},
+    { name: "Dead of Nuclear Winter", image: "media\dead_of_nuclear_winter.png"},
+    { name: "Ephemeral Enhancement", image: "media\ephemeral_enhancement.png"},
+    { name: "I'm Feeling Lucky", image: "media\im_feeling_lucky.png"},
+    { name: "Immolation Liquidation", image: "media\immolation_liquidation.png"},
+    { name: "Licensed Contractor", image: "media\licensed_contractor.png"},
+    { name: "Phoenix Up", image: "media\phoenix_up.png"},
+    { name: "Pop Shocks", image: "media\pop_shocks.png"},
+    { name: "Respin Cycle", image: "media\respin_cycle.png"}
+]
