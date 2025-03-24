a4c662cf1b20a4f1cb490b0bb457c5acc97a25db
Naveed-easv <navnou01@easv365.dk>
2025-03-24 13:28:57 +0100

    Begin work on todo list

---
+++ todo_list/main.js
@@ -0,0 +1,27 @@
+async function getData() {
+    const url = "https://dummyjson.com/todos";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Response status: ${response.status}");
+      }
+  
+      const json = await response.json();
+      console.log(json);
+      const listEl = document.getElementById("list");
+
+      for (const item of json.todos) {
+        let myClass = "uncompleted"
+        const statusMessage = item.completed
+            ? "✅"
+            : "❌";
+        if (item.completed === true) {
+            myClass = "completed"
+        }
+        listEl.innerHTML += `<li class="${myClass}">${item.todo} - status: ${statusMessage}</li>`
+    }
+    } catch (error) {
+      console.error(error.message);
+    }
+}
+getData();
